5b700695c5d83f6236aa322a8e0af4ef122f2927
unknown <netstantechnology@gmail.com>
2025-01-12 00:17:15 +0100

    Fixes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { MiniChart } from "react-ts-tradingview-widgets";
-import MarketOverview from "../components/cryptoWidget";
+import { MarketOverview } from "react-ts-tradingview-widgets";
 import TopGainers from "../components/topGainers";
 import VassetLoans from "../components/vassetLoans";
 
@@ -175,7 +175,39 @@ const Dashboard = () => {
             alignSelf: isSmallScreen ? "center" : "flex-start",
           }}
         >
-          <MarketOverview />
+          <div
+            style={{
+              width: isSmallScreen ? "350px" : "600px",
+              height: isSmallScreen ? "500px" : "400px",
+              overflow: "hidden",
+            }}
+          >
+            <MarketOverview
+              colorTheme="light"
+              width="100%"
+              height="100%"
+              tabs={[
+                {
+                  title: "Crypto",
+                  symbols: [
+                    { s: "BINANCE:BTCUSDT", d: "Bitcoin" },
+                    { s: "BINANCE:ETHUSDT", d: "Ethereum" },
+                    { s: "BINANCE:BNBUSDT", d: "Binance Coin" },
+                    { s: "BINANCE:XRPUSDT", d: "Ripple" },
+                    { s: "BINANCE:SOLUSDT", d: "Solana" },
+                  ],
+                },
+                {
+                  title: "Forex",
+                  symbols: [
+                    { s: "FX:EURUSD", d: "EUR/USD" },
+                    { s: "FX:GBPUSD", d: "GBP/USD" },
+                    { s: "FX:USDJPY", d: "USD/JPY" },
+                  ],
+                },
+              ]}
+            />
+          </div>
           <TopGainers />
         </div>
         <VassetLoans />
